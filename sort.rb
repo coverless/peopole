3f71e6ec039d5b0badcf25c69e05c099949d0110
swcraig <sebastian.w.craig@gmail.com>
2015-07-15 21:05:35 -0700

    Added AlchemyAPI, getResults() in sort.rb

--- sort.rb
+++ sort.rb
@@ -1,7 +1,28 @@
 # Sorts the file by the most results
 # Done in Ruby, because we aren't bad
 
+require 'yaml'
+
+# May not be needed
+require 'net/http'
+require 'open-uri'
+require 'openssl'
 require 'json'
+# Can be prettier
+require "./alchemyapi_ruby/alchemyapi.rb"
+
+
+# Queries Alchemy and returns the # of articles for each person
+# Writes to results.txt
+def getResults
+  apikey = File.open("config.yml") { |f| YAML.load(f)['ALCHEMYKEY'] }
+  alchemyapi = AlchemyAPI.new()
+  puts "Created AlchemyAPI"
+
+  # TODO -> MAKE THE MAGIC HAPPEN!
+
+end
+
 
 # Outputs top results in final.txt by number of tweets
 def sortResults
@@ -47,8 +68,11 @@ if ARGV[0] == "-p"
   sortPeople
 elsif ARGV[0] == "-t"
   sortResults
+elsif ARGV[0] == "-g"
+  getResults
 else
   puts "\nUSAGE: run 'ruby sort' with one of the following parameters"
   puts "\t-t (sort the results by # of tweets)"
   puts "\t-p (sort people.txt alphabetically)"
+  puts "\t-g (get the results)"
 end
