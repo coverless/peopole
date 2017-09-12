5479347c28d2a10654919dbcee2051e032fb2c74
swcraig <sebastian.w.craig@gmail.com>
2017-09-11 21:41:56 -0700

    Add spec for `Platform::Twitter#account_for`
    Adds a context for when there are no verified accounts returned from the
    search.

--- spec/platform/twitter_spec.rb
+++ spec/platform/twitter_spec.rb
@@ -3,18 +3,33 @@
 require_relative '../../platform/twitter'
 
 describe Platform::Twitter do
-  subject { described_class.new }
+  let(:twitter_platform) { described_class.new }
 
   describe '#account_for' do
-    let(:expected) { 'https://twitter.com/DILLONFRANCIS' }
-    it 'returns the correct account' do
-      expect(subject.account_for('Dillon Francis')).to eq(expected)
+    subject { twitter_platform.account_for(person) }
+
+    context 'when a verified account exists' do
+      let(:person) { 'Dillon Francis' }
+      let(:expected) { 'https://twitter.com/DILLONFRANCIS' }
+
+      it 'returns the correct account' do
+        expect(subject).to eq(expected)
+      end
+    end
+
+    context 'when there is no verified account' do
+      let(:person) { 'blueyhuey' }
+      let(:expected) { '' }
+
+      it 'returns an empty string' do
+        expect(subject).to eq(expected)
+      end
     end
   end
 
   describe '#count_for' do
     it 'returns an integer' do
-      expect(subject.count_for('Rick and Morty')).to be_an Integer
+      expect(twitter_platform.count_for('Rick and Morty')).to be_an Integer
     end
   end
 end
